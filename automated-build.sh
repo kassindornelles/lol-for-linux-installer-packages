e6e119d9304b0bd50f9cdda405735d92f7ff5068
kaßin <kassin.dornelles@gmail.com>
2023-10-06 10:25:53 -0300

    Remove Arch Git and improve automated script

--- automated-build.sh
+++ automated-build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+mkdir -p packages
+
+echo "BUILDING FOR ARCH LINUX"
 cd archlinux
 makepkg
-
+echo "BUILDING FOR FEDORA"
 cd ../fedora-rpm
 ./build_rpm.sh
-
+echo "BUILDING FOR UBUNTU LTS"
 cd ../ubuntu-LTS
 ./createdeb.sh
 
 cd ..
+echo "MOVING FILES TO PACKAGES FOLDER"
+mv archlinux/*.pkg.tar.zst packages/
+mv *.deb packages/
+mv fedora-rpm/*.rpm packages/
 
 echo "done"
+
 
